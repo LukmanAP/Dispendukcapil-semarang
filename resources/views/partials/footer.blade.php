f045818723ca91ec2f8a43e7a2ece594d7ff5897
ika <ikafauziahnadila@gmail.com>
2024-02-17 23:20:21 +0700

    change navbar and footer layout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,55 +4,49 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-6">
+          <div class="col-lg-4 col-md-8">
             <div class="footer-info">
-              <h3><img src="/assets/img/logo.png" alt="" width="150px" height="150px"></h3>
-              <p>
-              Kec. Gayamsari, Kota Semarang, Jawa Tengah .
-              </p>
+              <!-- <h3><img src="/assets/img/logo.png" alt="" width="100px" height="100px"></h3> -->
+              <h4>Kontak Kami</h4>
+              <p>Jl. Kanguru Raya No. 3</p>
+              <p>Semarang - 50161 - Jawa Tengah</p>
+              <p>Fax. 024 6707203</p>
               <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                <a href="https://wa.me/6285641604903/" target="_blank" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+                <a href=https://twitter.com/DUKCAPILKOTASMG target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="https://www.instagram.com/disdukcapilkotasemarang/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Menu</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="/">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="/antrian">Antrian</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="/contact">Contact</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="/login">Login</a></li>
-            </ul>
+          <div class="col-lg-4 col-md-8">
+            <div class="footer-info">
+              <!-- <h3><img src="/assets/img/logo.png" alt="" width="100px" height="100px"></h3> -->
+              <h4>Jam Pelayanan</h4>
+              <p>Senin - Kamis : 08.15 - 15.00</p>
+              <p>Jumat : 08.00 - 13.00 </p>
+              <p>( Istirahat Jumat: 11.30 - 12.30 )</p>
+              <p>Sabtu & Minggu : Libur</p>
+            </div>
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Jenis Layanan</h4>
-            <ul>
-              @foreach ($antrians as $antrian)
-                <li><i class="bx bx-chevron-right"></i> <a href="/antrian/">{{ $antrian->nama_layanan }}</a></li>
-              @endforeach
-            </ul>
+          <div class="col-lg-4 col-md-8">
+            <div class="footer-info">
+              <!-- <h3><img src="/assets/img/logo.png" alt="" width="100px" height="100px"></h3> -->
+              <h4>Tautan Terkait</h4>
+              <p><a href="https://www.lapor.go.id/" target="_blank">Lapor - Pemerintah Kota Semarang </a></p>
+              <p><a href="https://dukcapil.kemendagri.go.id/" target="_blank">Ditjen Kemendagri</a></p>
+              <p><a href="https://semarangkota.go.id/" target="_blank">Pemerintah Kota Semarang</a></p>
+            </div>
           </div>
-
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Dapatkan Update Informasi Terbaru</h4>
-            <p>Dapatkan Info terbaru dari Dispendukcapil Semarang dengan mengikuti Instagram kami</p>
-
-
-          </div>
-
         </div>
       </div>
     </div>
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>Sistem Antrian Online</span></strong>. All Rights Reserved
+      Copyright &copy; 2024 <strong><span>Sistem Antrian Online</span></strong>. All Rights Reserved
       </div>
     </div>
-  </footer><!-- End Footer -->
+  </footer>
+  <!-- End Footer -->
