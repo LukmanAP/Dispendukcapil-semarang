71f209d34461d8ba103d130e17d95b8d62a48af0
Lukman <lukmanagungprakoso@gmail.com>
2024-02-20 14:55:23 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,74 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        Antrian::create([
+            'nama_layanan' => 'Layanan E-KTP',
+            'kode'         => 'KTP',
+            'slug'         => 'layanan-e-ktp',
+            'batas_antrian'=> 20,
+            'deskripsi'    => 'Ambil antrian untuk mengurus perekaman E-KTP',
+            'persyaratan'  => 'Berkas yang dibawa Fotocopy KK, KTP-el asli yang lama, Surat keterangan hilang kepolisian',
+            'user_id'      => 1,
+        ]);
+
+        Antrian::create([
+            'nama_layanan' => 'Layanan Kartu Keluarga',
+            'kode'         => 'KK',
+            'slug'         => 'layanan-kartu-keluarga',
+            'batas_antrian'=> 20,
+            'deskripsi'    => 'Ambil antrian untuk mengurus Kartu Keluarga',
+            'persyaratan'  => 'Berkas yang dibawa Fotocopy KK',
+            'user_id'      => 1,
+        ]);
+
+        Antrian::create([
+            'nama_layanan' => 'Layanan Akta Kelahiran',
+            'kode'         => 'AKKEL',
+            'slug'         => 'layanan-akta-kelahiran',
+            'batas_antrian'=> 20,
+            'deskripsi'    => 'Ambil antrian untuk mengurus Akta Kelahiran',
+            'persyaratan'  => 'Berkas yang dibawa Fotocopy KK',
+            'user_id'      => 1,
+        ]);
+
+        Antrian::create([
+            'nama_layanan' => 'Layanan Surat Pindah',
+            'kode'         => 'SP',
+            'slug'         => 'layanan-surat-pindah',
+            'batas_antrian'=> 20,
+            'deskripsi'    => 'Ambil antrian untuk mengurus Surat Pindah',
+            'persyaratan'  => 'Berkas yang dibawa Fotocopy KK',
+            'user_id'      => 1,
+        ]);
+
+        Layanan::create([
+            'nama_layanan' => 'Layanan E-KTP',
+            'kode'         => 'KTP',
+            'deskripsi'    => 'Pelayanan dan pengurusan E-KTP',
+            'user_id'      => 1
+        ]);
+
+        Layanan::create([
+            'nama_layanan' => 'Layanan Kartu Keluarga',
+            'kode'         => 'KK',
+            'deskripsi'    => 'Pelayanan dan pengurusan Kart Keluarga (KK)',
+            'user_id'      => 1
+        ]);
+
+        Layanan::create([
+            'nama_layanan' => 'Layanan Akta Kelahiran',
+            'kode'         => 'AKKEL',
+            'deskripsi'    => 'Pelayanan dan pengurusan Akta Kelahiran',
+            'user_id'      => 1
+        ]);
+
+        Layanan::create([
+            'nama_layanan' => 'Layanan Surat Pindah',
+            'kode'         => 'KK',
+            'deskripsi'    => 'Pelayanan dan pengurusan Surat Pindah',
+            'user_id'      => 1
+        ]);
+
         User::create([
             'name'      => 'Admin',
             'email'     => 'admin@gmail.com',
