5c5ce738b77ec60abb77194e058ae9104d6b2c5e
ika <ikafauziahnadila@gmail.com>
2024-02-20 09:48:48 +0700

    update header layout

--- resources/views/partials/navbar-auth.blade.php
+++ resources/views/partials/navbar-auth.blade.php
@@ -2,22 +2,14 @@
 
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
-      <div class="d-flex justify-content-start">
-        <h1 class="logo me-auto me-lg-0"><a href="/"><img src="/assets/img/logo-2.png" alt="logo"></a></h1>
-      </div>
-      
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      <div class="container d-flex justify-content-end">
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto" href="/">Home</a></li>
-          <!-- <li><a class="nav-link scrollto me-3" href="/contact">Contact</a></li> -->
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-      </div>
+    <div class="container d-flex align-items-center justify-content-between">
+      <nav id="navbar" class="navbar ">
+        <div class="logo ">
+          <img src="/assets/img/logo-2.png" class="img-fluid" alt="logo">
+        </div>
+      </nav>
+
+      <a href="/" class="get-started-btn scrollto text-center">Home</a>
 
     </div>
   </header><!-- End Header -->
