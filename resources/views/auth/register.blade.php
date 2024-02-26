64ca66d9fa20d4bd46142c95b8f4acb3bb03e99b
ika <ikafauziahnadila@gmail.com>
2024-02-26 12:37:38 +0700

    fix bug hidden eye password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,41 +34,29 @@
                             <div class="input-group-append">
                                 <span class="input-group-text field-icon fa fa-fw fa-eye toggle-password" style="width: 30px;"></span>
                             </div>
+                            @error('password')
+                                <span class="invalid-feedback" style="border-radius: 20px;" role="alert">
+                                    <p>{{ $message }}</p>
+                                </span>
+                            @enderror
                         </div>
-
-                        <script>
-                            document.addEventListener('DOMContentLoaded', function() {
-                                const togglePassword = document.querySelector('.toggle-password');
-                                const password = document.querySelector('#password');
-
-                                togglePassword.addEventListener('click', function() {
-                                    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-                                    password.setAttribute('type', type);
-                                    this.classList.toggle('fa-eye-slash');
-                                });
-                            });
-                        </script>
-
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <p>{{ $message }}</p>
-                            </span>
-                        @enderror
                     </div>
 
                     <div class="form-group">
                         <div class="input-group">
-                            <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="password" name="password_confirmation" placeholder="Masukkan Password">
+                            <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="password1" name="password_confirmation" placeholder="Masukkan Password">
                             <div class="input-group-append">
                                 <span class="input-group-text field-icon fa fa-fw fa-eye toggle-password1" style="width: 30px; "></span>
                             </div>
+
+                            @error('password_confirmation')
+                                <span class="invalid-feedback" role="alert">
+                                    <p>{{ $message }}</p>
+                                </span>
+                            @enderror
                         </div>
                         
-                        @error('password_confirmation')
-                            <span class="invalid-feedback" role="alert">
-                                <p>{{ $message }}</p>
-                            </span>
-                        @enderror
+                        
                     </div>
 
                     <div class="row d-flex ">
@@ -91,8 +79,19 @@
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        const togglePassword1 = document.querySelector('.toggle-password1');
+        const togglePassword = document.querySelector('.toggle-password');
         const password = document.querySelector('#password');
+
+        togglePassword.addEventListener('click', function() {
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            this.classList.toggle('fa-eye-slash');
+        });
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const togglePassword1 = document.querySelector('.toggle-password1');
+        const password = document.querySelector('#password1');
 
         togglePassword1.addEventListener('click', function() {
             const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
